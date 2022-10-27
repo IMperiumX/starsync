0d474da288a4883551dacccc0c1c8dc65c94b1cb
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-10-27 06:44:02 +0200

    keep it secret

--- starsync/constants.py
+++ starsync/constants.py
@@ -1,10 +1,30 @@
-APIKEY = "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"
+# Environment variables
 
-URL_TEMPLATE = template = Template("https://api.github.com/user/starred/$repo")
+import os
+
+get_env = os.environ.get
+APIKEY = get_env("APIKEY")
+
+
+# String Templates
+
+from string import Template
+
+URL_TEMPLATE = Template("https://api.github.com/user/starred/$repo")
 
 HEADERS = headers = {
     "Authorization": f"Bearer {APIKEY}",
     "Accept": "application/vnd.github+json",
 }
 
+# API Endpoints
+
 STARRED_ENDPONT = "https://api.github.com/user/starred"
+
+# files realted
+
+from pathlib import Path
+
+PROJECT_PATH = Path(__file__).resolve().parent
+FILE_NAME = "repos.txt"
+FILE_PATH = PROJECT_PATH / FILE_NAME
