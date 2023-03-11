541745b4d94ef5f13d736c356dc31ef8b475e384
yusufadell <yusufadell.dev@gmail.com>
2023-03-11 17:20:26 +0200

    feat: add cli entrypoint to run starsync

--- starsync/cli.py
+++ starsync/cli.py
@@ -1,15 +1,20 @@
 """Console script for starsync."""
 import sys
 
+import asyncio
+from timeit import default_timer
+
 import click
+
+from .sync_unstart_repos import unstar_repos_async
 
 
 @click.command()
 def main(args=None):
-    """Console script for starsync."""
-    click.echo("Replace this message by putting your code into " "starsync.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+    start = default_timer()
+    asyncio.run(unstar_repos_async())
+    elapsed = default_timer() - start
+    click.echo(f"Elapsed Time: {elapsed:.2f} seconds")
 
 
 if __name__ == "__main__":
